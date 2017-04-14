d9251728c798157659307299f65a23a6b19fb551
Lincoln Brito <lincoln.sbrito@gmail.com>
2017-04-14 12:21:01 -0300

    update composer

---
+++ Creational/AbstractFactory/src/HtmlFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DesignPatterns\Creational\AbstractFactory;
+
+
+class HtmlFactory extends AbstractFactory
+{
+    public function createText(string $content): Text
+    {
+        return new HtmlText($content);
+    }
+}
---
+++ Creational/AbstractFactory/tests/AbstractFactoryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DesignPatterns\Creational\AbstractFactory;
+
+use PHPUnit\Framework\TestCase;
+
+class AbstractFactoryTest extends TestCase
+{
+    public function testCanCreateJsonText()
+    {
+        $this->assertEquals(1,1);
+    }
+}
