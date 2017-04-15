43aed5bb29ca407c1563755d2561b579ee572dac
Lincoln Brito <lincoln.sbrito@gmail.com>
2017-04-14 21:09:52 -0300

    add vehicle parts

---
+++ Creational/Builder/src/Director.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DesignPatterns\Creational\Builder;
+
+use DesignPatterns\Creational\Builder\Parts\Vehicle;
+
+/**
+ * Director é parte do padrão builder. Ele conhece a interface do builder
+ * e constrói um objeto complexo com ajuda do builder.
+ * Você também pode injetar muitos builders ao invés de um para constuir objetos mais complexos.
+ *
+ * Class Director
+ * @package DesignPatterns\Creational\Builder
+ */
+class Director
+{
+    public function build(BuilderInterface $builder): Vehicle
+    {
+        $builder->createVehicle();
+        $builder->addDoors();
+        $builder->addEngine();
+        $builder->addWheel();
+
+        return $builder->getVehicle();
+    }
+}
