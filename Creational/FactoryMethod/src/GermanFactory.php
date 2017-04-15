f1d8b7f48d43df8b4af4b825f09f58201c923e44
Lincoln Brito <lincoln.sbrito@gmail.com>
2017-04-14 22:33:22 -0300

    implement german factory

---
+++ Creational/FactoryMethod/src/GermanFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DesignPatterns\Creational\FactoryMethod;
+
+class GermanFactory extends FactoryMethod
+{
+    protected function createVehicle(string $type): VehicleInterface
+    {
+        switch ($type) {
+            case parent::CHEAP:
+                return new Bicycle();
+            case parent::FAST:
+                $carMercedes = new CarMercedes();
+                // podemos especializar a forma como quremos algum Vehicle concreto desde que conheçamos a classe
+                $carMercedes->addAMGTuning();
+
+                return $carMercedes;
+            default:
+                throw new \InvalidArgumentException("$type is not a valide vehicle");
+        }
+    }
+}
